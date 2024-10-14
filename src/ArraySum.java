5e2bf905700c9bafd5a5931506fc5c70312f4497
rumesh <rumeshk066@gmail.com>
2024-10-14 16:02:38 +0530

    ArraySum

--- src/ArraySum.java
+++ src/ArraySum.java
@@ -1,4 +1,24 @@
-
+import java.util.Scanner;
 
 public class ArraySum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int sum = 0;
+        for (int value : array) {
+            sum += value;
+        }
+
+        System.out.println("Sum of array elements: " + sum);
+        scanner.close();
+    }
 }
