f4f19ea6d40dd8d10381987361158f0667030ceb
rumesh <rumeshk066@gmail.com>
2024-10-14 20:01:58 +0530

    Add

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class A {
+    void print() {
+        System.out.printf("A");
+    }
+}
+
+class B extends A {
+    void print() {
+        System.out.printf("B");
+    }
+}
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        B b = new B();
+        b.print(); // Output: B
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        A a1 = new B();
+        a1.print(); // Output: A
+
+
+
     }
 }
