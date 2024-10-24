ebb4a8f879dfd474f413e326f7aacb1c52b0b629
rumesh <rumeshk066@gmail.com>
2024-10-24 11:17:26 +0530

    Exception Handling..1

--- src/Handling.java
+++ src/Handling.java
@@ -1,18 +1,26 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Handling {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a, b, c;
-        System.out.print("Enter first number: ");
-        a = scanner.nextInt();
-        System.out.print("Enter second number: ");
-        b = scanner.nextInt();
+
         try {
+            System.out.print("Enter first number: ");
+            a = scanner.nextInt();
+            System.out.print("Enter second number: ");
+            b = scanner.nextInt();
             c = a / b;
             System.out.println("Result: " + c);
         }
-        catch (Exception e) {
-            System.out.println("Error: Division by zero.");
+        catch (ArithmeticException e) {
+            System.out.println("Can not divide by zero");
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a number");
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a number");
         }
         System.out.println("Bye");
         scanner.close();
